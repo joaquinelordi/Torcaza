68ebfccb60327fb4f10505d88e8682ae00940829
TELECTRONICA\jelordi <jelordi@telectronicapeaje.com>
2025-07-03 08:54:58 -0300

    se agrega controlador para enlazar la comunicacion de la API con el socketServidor y el procesamiento de mensajes

---
+++ Torcaza/Controllers/CapaComunicacionAplicacionController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServidorTCP;
+
+namespace Torcaza.Controllers
+{
+    [ApiController]
+    [Route("api/canal")]
+    public class CapaComunicacionAplicacionController : Controller
+    {
+        private readonly TcpServer _tcpServer;
+
+        public CapaComunicacionAplicacionController(TcpServer tcpServer)
+        {
+            _tcpServer = tcpServer;
+        }
+
+        [HttpPost("envio")]
+        public async Task<IActionResult> ProcesarMensaje()
+        {
+            using var reader = new StreamReader(Request.Body);
+            var contenido = await reader.ReadToEndAsync();
+
+            if (string.IsNullOrWhiteSpace(contenido))
+                return BadRequest("El body está vacío.");
+
+            try
+            {
+                _tcpServer.ProcesarMensajeExterno(contenido);
+
+                return Ok(new
+                {
+                    success = true,
+                    message = "Mensaje recibido y procesado por el servidor TCP."
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    success = false,
+                    message = "Error al procesar el mensaje.",
+                    error = ex.Message
+                });
+            }
+        }
+    }
+}
