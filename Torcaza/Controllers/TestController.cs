c5bd84e60e5567ece7e3311d85502d8cf85699fa
TELECTRONICA\jelordi <jelordi@telectronicapeaje.com>
2025-08-25 20:02:58 -0300

    Se agrega proyecto y referencias para tests

---
+++ Torcaza/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Entidades;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using ServidorTCP;
+
+namespace Torcaza.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : Controller
+    {
+        private readonly TcpServer _tcpServer;
+        private readonly HandlerJWT _handlerJWT;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public TestController(TcpServer tcpServer, HandlerJWT handlerJWT)
+        {
+            _tcpServer = tcpServer;
+            _handlerJWT = handlerJWT;
+        }
+
+        [HttpGet("test")]
+        public async Task<IActionResult> TestearMetodos()
+        {
+            // Test punto dentro de cerco
+            _tcpServer.TestUbicacionDentroFueraDeCerco();
+
+
+            return Ok(new
+            {
+                success = true,
+                mensaje = "API Test funcionando correctamente.",
+                fecha = DateTime.UtcNow
+            });
+
+        }
+    }
+}
